74830d6f4113c4d84470d656bce3b47f5a1eabce
ruixiaoguo <grx0917@sina.com>
2018-08-09 11:07:42 +0800

    IJK拉流

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, "7.0"
+
+pod "GPUImage"
+
+target :ijkplayerDemo, :exclusive => true do
+  # pod 'Kiwi'
+end
---
+++ ijkplayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ijkplayerDemo
+//
+//  Created by lihongfeng on 16/4/22.
+//  Copyright © 2016年 wanglei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
